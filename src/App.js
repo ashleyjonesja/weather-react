cce52e7d65713d86805fb1d3aa0e0c6ec7c04b49
ashleyjonesja <ashleyjonesja@gmail.com>
2021-06-20 18:49:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,3 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Weather from "./Weather";
-
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
