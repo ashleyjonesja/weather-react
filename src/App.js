e2d98704d77b940ff1907a37ef5c50f0f9207860
ashleyjonesja <ashleyjonesja@gmail.com>
2021-06-29 22:56:52 +0100

    Update reactdom import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
 import Weather from "./Weather.js";
 import "bootstrap/dist/css/bootstrap.css";
@@ -8,12 +7,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather defaultCity="London" />
-      <footer>
-        This project was coded by Jacinda Ashley-Jones and is{" "}<a href="https://github.com/ashleyjonesja/weather-react" target="_blank" rel="noreferrer">
-        open-sourced on GitHub</a>
-      </footer>
+        <Weather defaultCity="London" />
+        <footer>
+          This project was coded by Jacinda Ashley-Jones and is{" "}
+          <a
+            href="https://github.com/ashleyjonesja/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
-  }
+}
